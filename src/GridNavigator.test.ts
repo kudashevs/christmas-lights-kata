ca1387353a7bc77e61b0be1c6834631536c79770
Sergey Kudashev <kudashevs@gmail.com>
2023-12-15 20:31:36 +0300

    Update GridNavigator add a process area functionality

--- src/GridNavigator.test.ts
+++ src/GridNavigator.test.ts
@@ -1,8 +1,9 @@
+import {GridArea} from './GridArea';
 import {GridNavigator} from './GridNavigator';
 import {LightsGrid} from './LightsGrid';
 
 describe('GridNavigator test suite', () => {
-  it('can invoke a method on a grid', () => {
+  it('can invoke a method on a whole grid', () => {
     const grid = new LightsGrid(5, 5);
 
     for (let i = 0; i < grid.getHeight(); i++) {
@@ -17,6 +18,29 @@ describe('GridNavigator test suite', () => {
     for (let i = 0; i < grid.getHeight(); i++) {
       for (let j = 0; j < grid.getWidth(); j++) {
         expect(grid.getLight(i, j).isOn()).toBe(true);
+      }
+    }
+  });
+
+  it('can invoke a method on an area', () => {
+    const grid = new LightsGrid(5, 5);
+
+    for (let i = 0; i < grid.getHeight(); i++) {
+      for (let j = 0; j < grid.getWidth(); j++) {
+        expect(grid.getLight(i, j).isOn()).toBe(false);
+      }
+    }
+
+    const navigator = new GridNavigator();
+    navigator.invoke(grid, 'turnOn', new GridArea(0, 0, 4, 0));
+
+    for (let i = 0; i < grid.getHeight(); i++) {
+      for (let j = 0; j < grid.getWidth(); j++) {
+        if (j === 0) {
+          expect(grid.getLight(i, j).isOn()).toBe(true);
+        } else {
+          expect(grid.getLight(i, j).isOn()).toBe(false);
+        }
       }
     }
   });
--- src/GridNavigator.ts
+++ src/GridNavigator.ts
@@ -1,8 +1,31 @@
+import {GridArea} from './GridArea';
 import {LightsGrid} from './LightsGrid';
 import {Navigator} from './Navigator';
 
 export class GridNavigator implements Navigator {
-  public invoke(grid: LightsGrid, method: string) {
+  public invoke(grid: LightsGrid, method: string, area: GridArea = undefined): void {
+    if (area) {
+      this.processArea(grid, method, area);
+      return;
+    }
+
+    this.processRaw(grid, method);
+  }
+
+  private processArea(grid: LightsGrid, method: string, area: GridArea): void {
+    const leftX: number = area.getLeftX();
+    const leftY: number = area.getLeftY();
+    const rightX: number = area.getRightX();
+    const rightY: number = area.getRightY();
+
+    for (let i = leftX; i <= rightX; i++) {
+      for (let j = leftY; j <= rightY; j++) {
+        grid.getLight(i, j)[method]();
+      }
+    }
+  }
+
+  private processRaw(grid: LightsGrid, method: string): void {
     for (let i = 0; i < grid.getHeight(); i++) {
       for (let j = 0; j < grid.getWidth(); j++) {
         grid.getLight(i, j)[method]();
